8ac6c3cba38460277634fc13b9e246899cb2fbb3
ashehovtsov <ashehovtsov@itera-research.com>
2016-11-28 15:42:14 +0200

    add window

--- js/script.js
+++ js/script.js
@@ -1,31 +1,34 @@
-var myLayout = new dhtmlXLayoutObject({
-  parent: "layoutObj",
-  pattern: "3E",   // <-- pattern
-    
-  offsets: {          // optional, offsets for fullscreen init
-       top:    10,     // you can specify all four sides
-       right:  10,     // or only the side where you want to have an offset
-       bottom: 10,
-       left:   10
-   },
+//var myLayout = new dhtmlXLayoutObject({
+//  parent: "layoutObj",
+//  pattern: "3E",   // <-- pattern
+//    
+//  offsets: {          // optional, offsets for fullscreen init
+//       top:    10,     // you can specify all four sides
+//       right:  10,     // or only the side where you want to have an offset
+//       bottom: 10,
+//       left:   10
+//   },
+//
+//   cells: [    // optional, cells configuration according to the pattern
+//               // you can specify only the cells you want to configure
+//               // all params are optional
+//       {
+//           id:             "a",        // id of the cell you want to configure
+//           text:           "Text",     // header text
+//           collapsed_text: "Text 2",   // header text for a collapsed cell
+//           header:         false,      // hide header on init
+//           width:          100,        // cell init width
+//           height:         100,        // cell init height
+//           collapse:       true,        // collapse on init
+//           fix_size:       [true,null] // fix cell's size, [width,height]
+//       }
+//
+//   ]
+//});
 
-   cells: [    // optional, cells configuration according to the pattern
-               // you can specify only the cells you want to configure
-               // all params are optional
-       {
-           id:             "a",        // id of the cell you want to configure
-           text:           "Text",     // header text
-           collapsed_text: "Text 2",   // header text for a collapsed cell
-           header:         false,      // hide header on init
-           width:          100,        // cell init width
-           height:         100,        // cell init height
-           collapse:       true,        // collapse on init
-           fix_size:       [true,null] // fix cell's size, [width,height]
-       }
-
-   ]
-});
-
+var myWins = new dhtmlXWindows()
+myWins.createWindow("about", 20, 30, 300, 200);
+myWins.attachViewportTo(document.getElementsByTagName('body'));
 
   gantt.init("gantt_here");
 
